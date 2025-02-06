4ee02734fcf7c33c93a8a3aa5e8e841546ae3d0d
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-02-06 17:41:49 +0000

    adding my CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,7 @@
 // import React from 'react'
 import HeroImage from '../assets/FaisalDrip.jpg'
+import MyCV from '../assets/MY CV.pdf';
+
 const Hero = () => {
   return (
     <div className="bg-black text-white text-center py-16">
@@ -21,11 +23,13 @@ const Hero = () => {
           transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
           Contact With Me
         </button>
-        <button 
+        <a href={MyCV} 
+          target="_blank" 
+          rel="noopener noreferrer"
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline 
           transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
             Resume
-          </button>
+        </a>
       </div>
     </div>
   )
